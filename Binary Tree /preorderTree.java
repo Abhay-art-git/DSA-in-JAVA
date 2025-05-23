7f3a25792be1d1857c284bcdcb6d1c774e0d2349
Abhay Mahto <abhaymahto0533@gmail.com>
2025-05-23 22:51:22 +0530

    Create preorderTree.java

---
+++ Binary Tree /preorderTree.java
@@ -0,0 +1,43 @@
+public class preorderTree{
+    static class Node{ // creating node 
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {  // constructor
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }    
+
+    }
+
+    static class BinaryTree{    // binary tree
+        static int idx = -1;    // index for track nodes
+        public static Node buildtree(int Nodes[]){ 
+            idx++;    // idx=0
+            if(Nodes[idx]==-1){
+                return null;  // left and right node are not permited
+            }
+
+            Node newnode = new Node(Nodes[idx]);  // new node create
+            newnode.left = buildtree(Nodes);   // left subtree
+            newnode.right = buildtree(Nodes);  // right subtree   
+             // = is used to join the nodes and the tree
+
+            return newnode;
+
+
+        }
+    }
+    public static void main(String[] args) {
+        int Node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree(); // we create a object for static BinaryTree class 
+        Node root = tree.buildtree(Node); // we store tree node 
+
+        System.out.println(root.data);  // print node of binary tree
+
+
+         
+    }
+}
