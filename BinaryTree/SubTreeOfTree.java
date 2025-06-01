d52e660d8860f28340ead7240cc683f2c5783186
Abhay Mahto <abhaymahto0533@gmail.com>
2025-06-02 00:24:26 +0530

    Create SubTreeOfTree.java

---
+++ BinaryTree/SubTreeOfTree.java
@@ -0,0 +1,79 @@
+public class SubTreeOfTree {
+    static class Node{
+        int data ;
+        Node left ;
+        Node right ;
+
+        public  Node(int data ){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+            
+        }
+    }
+    public static boolean isIdentical( Node node  , Node subRoot){
+        if(node  == null && subRoot == null){
+            return true;
+        }else if(node == null || subRoot == null || node.data !=subRoot.data){
+            return false;
+
+        }
+        if(!isIdentical(node.left , subRoot.left)){
+            return false;
+
+        }
+        if(!isIdentical(node.right, subRoot.right)){
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean Subtree( Node Root , Node subRoot){
+        if(Root == null){
+            return false;
+        }
+        if(Root.data == subRoot.data){
+            if(isIdentical(Root , subRoot)){
+                return true;
+            }
+        }
+        
+        return Subtree(Root.left, subRoot) || Subtree(Root.right, subRoot);
+
+    }
+    public static void main(String[] args) {
+         /*
+         *       1
+         *     /   \
+         *    2     3 
+         *   / \   / \
+            4   5 6   7
+
+         */ 
+        
+         Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        /*      2
+         *     / \
+         *    4   5 
+         */
+ 
+         Node subRoot = new Node(2);
+         subRoot.left = new Node(4);
+         subRoot.right = new Node(5);
+
+        System.out.println(Subtree(root, subRoot)); 
+
+
+
+         
+    }
+    
+}
