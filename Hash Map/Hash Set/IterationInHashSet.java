91f85023f4e28b9e52db09eba0eb728ce7945a98
Abhay Mahto <abhaymahto0533@gmail.com>
2025-06-26 22:37:28 +0530

    Create IterationInHashSet.java

---
+++ Hash Map/Hash Set/IterationInHashSet.java
@@ -0,0 +1,26 @@
+package HashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class IterationInHashSet {
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<>();
+        hs.add("delhi");
+        hs.add("noida");
+        hs.add("bangluru");
+        hs.add("punjab");
+
+
+        Iterator it = hs.iterator();  // its a method used for set iteration
+
+      while (it.hasNext()) {
+        System.out.println(it.next()); 
+          
+      }
+          
+
+    }
+
+    
+}
